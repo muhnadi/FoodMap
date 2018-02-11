1992c2b05b69074144ea57cd95ea7bbf5da1045a
team-nerdgeeks <greekycoder@gmail.com>
2018-02-11 07:16:25 +0600

    td

--- app/src/main/java/com/nerdgeeks/foodmap/app/AppConfig.java
+++ app/src/main/java/com/nerdgeeks/foodmap/app/AppConfig.java
@@ -8,19 +8,7 @@ package com.nerdgeeks.foodmap.app;
 public final class AppConfig {
 
     public static final String TAG = "foodmap";
-
-
-    //Key for nearby places json from google
-    public static final String STATUS = "status";
-    public static final String OK = "OK";
-    public static final String ZERO_RESULTS = "ZERO_RESULTS";
-    public static final String ROUTES = "routes";
-    public static final String OVERVIEW_POLYLINE = "overview_polyline";
-    public static final String POINTS = "points";
-
-    public static final String GOOGLE_MAP_API_KEY = "AIzaSyCqf1CgILEHcFSDfF_EPu2alMquq8eWvo0";
-
-
+    public static final String GOOGLE_MAP_API_KEY = "AIzaSyABo9TuqbRWLetmzyTYXOavNq1_ECTFUZA";
     public static final String INTERNET_ERROR = "Unable to resolve host, No internet access";
 
 }
